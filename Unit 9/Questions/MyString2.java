bfe7ded6254859e8c642e097df12c2174f647588
erkam_fedora <erkamuzuncayir@gmail.com>
2023-02-23 23:44:49 +0300

    Start to solve solution of Exercise_9_24.java

---
+++ Unit 9/Questions/MyString2.java
@@ -0,0 +1,19 @@
+// Chapter 9 - Exercise 24 | MyString2
+
+/*
+public MyString2(String s);
+public int compare(String s);
+public MyString2 substring(int begin);
+public MyString2 toUpperCase();
+public char[] toChars();
+public static MyString2 valueOf(boolean b);
+*/
+
+public class MyString2
+{
+	String internedString;
+	
+	public MyString2( String s )
+	{
+	
+	}
